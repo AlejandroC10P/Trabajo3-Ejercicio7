7177ce50252ecdf46cf0a1b413f6eee2a9c471d1
Alejandro Castro <111087081+AlejandroC10P@users.noreply.github.com>
2023-04-17 16:53:56 -0500

    Add files via upload

---
+++ Ejercicio7.java
@@ -0,0 +1,8 @@
+package Capitulo_4;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        InterfazEjercicio7 form = new InterfazEjercicio7();
+        form.setVisible(true);
+    }   
+}
